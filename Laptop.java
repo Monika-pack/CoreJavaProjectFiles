72e5b517bd08a0cc8530bc0f5989bc7f6fce64eb
Monika-pack <98116508+Monika-pack@users.noreply.github.com>
2022-04-30 08:22:35 -0400

    Add files via upload

--- Laptop.java
+++ Laptop.java
@@ -1,15 +1,37 @@
-package Laptop;
-import org.springframework.stereotype.Component;
+package oneTOmany_manyTOone;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+@Entity
 
-@Component("LaptopBean") //College collegeBean=new CollegeBean
+
 public class Laptop {
+	@Id
+	private int lid;
+	private String lname;
+	@ManyToOne  //many laptop will have one student
+	private Student st;
 	
-
-
-	
-	
-	  public void test() {
-		  System.out.println("Testing class method");
-	  }
+	public Student getSt() {
+		return st;
 	}
+	public void setSt(Student st) {
+		this.st = st;
+	}
+	public int getLid() {
+		return lid;
+	}
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	
+
+}
 
--- Student.java
+++ Student.java
@@ -1,14 +1,36 @@
-package spring_injection_setget;
+package oneTOmany_manyTOone;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
 
 public class Student {
-	private int id;
-	private String name;
-	private float fees;
-	public int getId() {
-		return id;
+	@Id
+	int rollno;
+	String name;
+	int marks;
+ @OneToMany(mappedBy = "st")  //one student will have multiple laptop
+	//@OneToMany
+	List<Laptop> lp=new ArrayList<Laptop>();
+	
+	
+	public List<Laptop> getLp() {
+		return lp;
 	}
-	public void setId(int id) {
-		this.id = id;
+	public void setLp(List<Laptop> lp) {
+		this.lp = lp;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
 	}
 	public String getName() {
 		return name;
@@ -16,16 +38,17 @@ public class Student {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public float getFees() {
-		return fees;
+	public int getMarks() {
+		return marks;
 	}
-	public void setFees(float fees) {
-		this.fees = fees;
+	public void setMarks(int marks) {
+		this.marks = marks;
 	}
-	public void display() {
-		System.out.println("Name"+name);
-		System.out.println("id"+id);
-		System.out.println("Fees"+fees);
+	
+	@Override
+	public String toString() {
+		return "Student [rollno=" + rollno + ", name=" + name + ", marks=" + marks + ", lp=" + lp + "]";
+	}
+}
 
-	}
-	}
+
